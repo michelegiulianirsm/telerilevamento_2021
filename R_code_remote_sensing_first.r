65d357f44c8232496f180e6640adf525c268c4b2
michelegiulianirsm <83698154+michelegiulianirsm@users.noreply.github.com>
2021-12-06 16:20:05 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,5 +1,10 @@
 # Primo codice di R
+ install.packages("raster")
+library(raster)
 setwd("C:/lab/")
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 p224r63_2011
 plot(p224r63_2011)
+# Cambio colore
+Colori <- colorRampPalette(c("black","grey","light grey")) (100)
+plot(p224r63_2011, col=Colori)
